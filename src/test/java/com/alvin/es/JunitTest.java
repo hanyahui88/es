df62d6d74c6a4f903a6f1438dd60781d000b4e65
alvin <hanyahui88@163.com>
2018-01-29 19:46:36 +0800

    test case is success

--- src/test/java/com/alvin/es/JunitTest.java
+++ src/test/java/com/alvin/es/JunitTest.java
@@ -3,12 +3,16 @@ package com.alvin.es;
 
 import com.alvin.es.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Test;
 
+import java.net.InetAddress;
+import java.util.Map;
 import java.util.Objects;
 
 public class JunitTest {
@@ -20,12 +24,18 @@ public class JunitTest {
         user.setAddress("beijingshi");
         user.setAge(28);
         user.setName("alvin");
+        Map map= Maps.newHashMap();
+        map.put("address","beijing");
+        map.put("age","29");
+        map.put("name","alvin111111");
 
         try {
             Settings settings = Settings.builder()
                     .put("cluster.name", "my-application").build();
             client = new PreBuiltTransportClient(settings);
-            IndexResponse indexResponse = client.prepareIndex("my_index", "user").setSource(user).get();
+            InetAddress inetAddress=InetAddress.getByAddress(new byte[]{127,0,0,1});
+            client.addTransportAddress(new TransportAddress(inetAddress, 9300));
+            IndexResponse indexResponse = client.prepareIndex("my_index", "user","1").setSource(map).get();
             System.out.println(indexResponse.getId());
             System.out.println(objectMapper.writeValueAsString(indexResponse.getResult()));
             System.out.println(objectMapper.writeValueAsString(indexResponse.getShardInfo()));
